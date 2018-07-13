6becf5faf5a14d041f56ee7350b7d133208684b6
Artaris <i@artaris.cn>
2018-07-13 11:14:07 +0800

    292. Nim Game
    292. Nim Game

--- src/question/NimGame.java
+++ src/question/NimGame.java
@@ -11,4 +11,19 @@ package question;
  * @author : rick
  */
 class NimGame {
+
+    public static void main(String[] args) {
+        //获取开始时间
+        long startTime = System.currentTimeMillis();
+
+
+
+        //获取结束时间
+        long endTime = System.currentTimeMillis();
+        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+    }
+
+    public static boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
 }
