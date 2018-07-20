041c60c7dd0e457dbdeb5eafd9acd9011c03e61f
ArtarisCN <i@artaris.cn>
2018-07-20 12:45:44 +0800

    404. Sum of Left Leaves
    404. Sum of Left Leaves

--- src/question/SumOfLeftLeaves.java
+++ src/question/SumOfLeftLeaves.java
@@ -7,13 +7,42 @@ import model.TreeNode;
  * LeetCode
  * 2018.07.20.上午11:47
  *
+ * 404. Sum of Left Leaves
+ * https://leetcode.com/problems/sum-of-left-leaves/description/
+ *
  * @author : rick
  */
 class SumOfLeftLeaves {
 
+    public static void main(String[] args) {
 
 
-    public int sumOfLeftLeaves(TreeNode root) {
+        //获取开始时间
+        long startTime = System.currentTimeMillis();
+
+
+        //获取结束时间
+        long endTime = System.currentTimeMillis();
+        System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+    }
+
+
+    public static int sumOfLeftLeaves(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null) {
+            return sumOfLeftLeaves(root.right);
+        }
+
+        if (root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        ;
 
     }
 }
