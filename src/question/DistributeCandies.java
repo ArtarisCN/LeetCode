3bf596b350114279e82c0d2a96ae26019fd371ae
Rick Lee <artaris@sina.com>
2018-10-25 10:54:57 +0800

    575. Distribute Candies
    575. Distribute Candies

--- src/question/DistributeCandies.java
+++ src/question/DistributeCandies.java
@@ -1,10 +1,13 @@
 package question;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * question
  * LeetCode
  * 2018.07.30.下午6:36
- *
+ * <p>
  * 575. Distribute Candies
  * https://leetcode.com/problems/distribute-candies/description/
  *
@@ -16,19 +19,52 @@ class DistributeCandies {
         //获取开始时间
         long startTime = System.currentTimeMillis();
 
+        System.out.println(distributeCandies(new int[]{1,1}));
 
         //获取结束时间
         long endTime = System.currentTimeMillis();
         System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
     }
 
-    public int distributeCandies(int[] candies) {
+    public static int distributeCandies(int[] candies) {
+
+        Set<Integer> sister = new HashSet<>();
+        Set<Integer> bro = new HashSet<>();
+
+        int l = candies.length / 2, s = 0, b = 0;
 
         for (int candy : candies) {
+            if (s == l) {
+                return sister.size();
+            }
 
+            if (!sister.contains(candy)) {
+                sister.add(candy);
+                s++;
+            } else {
+                if (bro.contains(candy)) {
+                    switch (s - b) {
+                        case -1:
+                            s++;
+                            break;
+                        case 1:
+                            b++;
+                            break;
+                        case 0:
+                            b++;
+                            break;
+                        default:
+                            b++;
+                            break;
+                    }
+                } else {
+                    bro.add(candy);
+                    b++;
+                }
+            }
         }
 
-        return 0;
+        return sister.size();
     }
 
 
